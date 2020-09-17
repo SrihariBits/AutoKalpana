d068d1d9ea280ab067f13d072f95a020f0e8744c
SrihariBits <47473205+SrihariBits@users.noreply.github.com>
2020-09-17 09:33:39 +0530

    Updated raga dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,20 @@
 dictionary = {
-    "mohanam": {"s": 6, "r": 7, "g": 8, "p": 9, "d": 10, "S": 11, "R": 12, "G": 13, "P": 14, "D": 15}
+    # Aro: S R G P D S
+    # Ava: S D P G R S
+    "mohanam": {"s": 6, "r": 7, "g": 8, "p": 9, "d": 10, "S": 11, "R": 12, "G": 13, "P": 14, "D": 15},
+    # Aro: S R G M P D N S
+    # Ava: S N D P M G R S
+    "kalyani": {"s": 8, "r": 9, "g": 10, "m": 11, "p": 12, "d": 13, "n": 14, "S": 15, "R": 16, "G": 17, "M": 18, "P": 19, "D": 20, "N": 21},
+    # Aro: S R G M P D N S
+    # Ava: S N D P M G R S
+    "panthuvarali": {"s": 8, "r": 9, "g": 10, "m": 11, "p": 12, "d": 13, "n": 14, "S": 15, "R": 16, "G": 17, "M": 18, "P": 19, "D": 20, "N": 21},
+    # Aro: S R M P D S
+    # Ava: S N D P M G R S
+    "saveri": {"s": 8, "r": 9, "g": 10, "m": 11, "p": 12, "d": 13, "n": 14, "S": 15, "R": 16, "G": 17, "M": 18, "P": 19, "D": 20, "N": 21},
+    # Aro: S R G M P D N S
+    # Ava: S N D P M G R S
+    "shankarabharanam": {"s": 8, "r": 9, "g": 10, "m": 11, "p": 12, "d": 13, "n": 14, "S": 15, "R": 16, "G": 17, "M": 18, "P": 19, "D": 20, "N": 21},
+    # Aro: S R G M P D N S
+    # Ava: S N D P M G R S
+    "thodi": {"s": 8, "r": 9, "g": 10, "m": 11, "p": 12, "d": 13, "n": 14, "S": 15, "R": 16, "G": 17, "M": 18, "P": 19, "D": 20, "N": 21}
 }
